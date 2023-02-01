35327b02c057f32e0074e08d23ce1d9f54a81497
SubediLijesh <Subedilijesh@gmail.com>
2023-02-01 07:34:52 +0545

    modify

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -20,7 +20,7 @@ class _ChatScreenState extends State<ChatScreen> {
 
   @override
   void initState() {
-    // TODO: implement initState
+    
     super.initState();
     getCurrentUser();
   }
--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -4,7 +4,7 @@ import '../components/RoundedButton.dart';
 import '../components/TextInput.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'chat_screen.dart';
-import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
+
 
 class LoginScreen extends StatefulWidget {
   static String id = 'login_screen';
